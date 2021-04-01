13e874edbd5c8d8a31dc90fd0cc81d8fcffd0d5d
fridoling <fridoling@hotmail.com>
2021-04-01 10:46:30 +0000

    classify matches with left 'N' as canonical

--- R/scanning.R
+++ R/scanning.R
@@ -507,16 +507,16 @@ getMatchTypes <- function(x, seed){
   seed6 <- substr(seed,2,7)
   seedGb6 <- paste0(substr(seed,2,3),"G",substr(seed,4,7))
   seedGb7 <- paste0(substr(seed,1,3),"G",substr(seed,4,7))
-  y[grep(paste0("[ACGT][ACGT]",substr(seed,3,8)),x)] <- 2L # 6mer-a1
-  y[grep(paste0(substr(seed,1,6),"[ACGT][ACGT]"),x)] <- 3L # 6mer-m8
+  y[grep(paste0("[ACGTN][ACGTN]",substr(seed,3,8)),x)] <- 2L # 6mer-a1
+  y[grep(paste0(substr(seed,1,6),"[ACGTN][ACGTN]"),x)] <- 3L # 6mer-m8
   if(substr(seedGb6,2,7)!=seed6)
     y[grep(seedGb6,x,fixed=TRUE)] <- 4L # g-bulged 6mer
-  y[grep(paste0("[ACGT]",substr(seed,2,7)),x)] <- 5L # 6mer
+  y[grep(paste0("[ACGTN]",substr(seed,2,7)),x)] <- 5L # 6mer
   if(seedGb6!=substr(seed,1,7)){
     y[grep(paste0(seedGb6,"A|",seedGb7),x)] <- 6L # g-bulged 7mer
     y[grep(paste0(seedGb7,"A"),x,fixed=TRUE)] <- 7L # g-bulged 8mer
   }
-  y[grep(paste0("[ACGT]",substr(seed,2,8)),x)] <- 8L # 7mer-a1
+  y[grep(paste0("[ACGTN]",substr(seed,2,8)),x)] <- 8L # 7mer-a1
   y[grep(substr(seed,1,7),x,fixed=TRUE)] <- 9L # 7mer-m8
   y[grep(seed,x,fixed=TRUE)] <- 10L # 8mer
   factor(y, levels=10L:1L, labels=.matchLevels())
