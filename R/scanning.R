b1c187daccccf70c6d7bebb01251a609193ea899
plger <pl.germain@gmail.com>
2021-04-12 12:25:49 +0000

    moved functions to scanMiRApp

--- R/scanning.R
+++ R/scanning.R
@@ -49,7 +49,7 @@ findSeedMatches <- function( seqs, seeds, shadow=0L, onlyCanonical=FALSE,
                              minDist=7L, p3.extra=FALSE, 
                              p3.params=list(maxMirLoop=5L, maxTargetLoop=9L, 
                                             maxLoopDiff=4L, mismatch=TRUE),
-                             agg.params=.defaultAggParams(),
+                             agg.params=.defaultAggParams(), writeToDir=NULL,
                              ret=c("GRanges","data.frame","aggregated"), 
                              BP=NULL, verbose=NULL, n_seeds=NULL, ...){
   p3.params <- .check3pParams(p3.params)
@@ -529,84 +529,6 @@ getMatchTypes <- function(x, seed){
   c("7mer","7mer","6mer","g-bulged 7mer","g-bulged 6mer","6mer","g-bulged 6mer",
     "6mer-m8","non-canonical","non-canonical")
 }
-  
-
-#' runFullScan
-#' 
-#' @export
-runFullScan <- function(species, mods=NULL, UTRonly=TRUE, shadow=15, cores=8, maxLogKd=c(-0.3,-0.3), save.path=NULL, ...){
-  message("Loading annotation")
-  suppressPackageStartupMessages({
-    library(ensembldb)
-    library(AnnotationHub)
-    library(BSgenome)
-    library(BiocParallel)
-  })
-  ah <- AnnotationHub()
-  species <- match.arg(species, c("mmu","hsa","rno"))
-  if(species=="hsa"){
-    genome <- BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38
-    if(is.null(mods)) mods <- readRDS(file = "/mnt/schratt/miRNA_KD/Data_Output/mods_hsa_comp.rds")
-    ahid <- rev(query(ah, c("EnsDb", "Homo sapiens"))$ah_id)[1]
-  }else if(species=="mmu"){
-    genome <- BSgenome.Mmusculus.UCSC.mm10::BSgenome.Mmusculus.UCSC.mm10
-    if(is.null(mods)) mods <- readRDS(file = "/mnt/schratt/miRNA_KD/Data_Output/mods_mmu_comp.rds")
-    ahid <- rev(query(ah, c("EnsDb", "Mus musculus"))$ah_id)[2]
-  }else if(species=="rno"){
-    genome <- BSgenome.Rnorvegicus.UCSC.rn6::BSgenome.Rnorvegicus.UCSC.rn6
-    if(is.null(mods)) mods <- readRDS(file = "/mnt/schratt/miRNA_KD/Data_Output/mods_rno_comp.rds")
-    ahid <- rev(query(ah, c("EnsDb", "Rattus norvegicus"))$ah_id)[1]
-  }
-  ensdb <- ah[[ahid]]
-  seqlevelsStyle(genome) <- "Ensembl"
-  
-  # restrict to canonical chromosomes
-  canonical_chroms <- seqlevels(genome)[!grepl('_', seqlevels(genome))]
-  filt <- SeqNameFilter(canonical_chroms)
-  
-  message("Extracting transcripts")
-  grl_UTR <- suppressWarnings(threeUTRsByTranscript(ensdb, filter=filt))
-  seqs <- extractTranscriptSeqs(genome, grl_UTR)
-  utr.len <- lengths(seqs)
-  names(utr.len) <- names(seqs)
-  if(!UTRonly){
-    grl_ORF <- cdsBy(ensdb, by="tx", filter=filt)
-    seqs_ORF <- extractTranscriptSeqs(genome, grl_ORF)
-    tx_info <- data.frame(strand=unlist(unique(strand(grl_ORF))))
-    orf.len <- lengths(seqs_ORF)
-    names(orf.len) <- names(grl_ORF)
-    tx_info$ORF.length <- orf.len[row.names(tx_info)]
-    seqs_ORF[names(seqs)] <- xscat(seqs_ORF[names(seqs)],seqs)
-    seqs <- seqs_ORF
-    rm(seqs_ORF)
-    mcols(seqs)$ORF.length <- orf.len[names(seqs)]
-  }else{
-    tx_info <- data.frame(strand=unlist(unique(strand(grl_UTR))))
-  }
-  tx_info$UTR.length <- utr.len[row.names(tx_info)]
-  
-  message("Scanning with ", cores, " cores")
-  if(cores>1){
-    BP <- MulticoreParam(cores, progress=TRUE)
-  }else{
-    BP <- SerialParam()
-  }
-  m <- findSeedMatches(seqs, mods, shadow=shadow, maxLogKd=maxLogKd, BP=BP, ...)
-  
-  if(is(m, "GRanges")) {
-    metadata(m)$tx_info <- tx_info
-    metadata(m)$ah_id <- ahid
-  } else {
-    attr(m, "tx_info") <- tx_info
-    attr(m, "ah_id") <- ahid
-  }
-  if(is.null(save.path)) return(m)
-  else saveRDS(m, file=save.path)
-  rm(m)
-  gc()
-  message("Saved in: ", save.path)
-}
-
 
 .defaultAggParams <- function(){
   list(ag=-4.863126,
