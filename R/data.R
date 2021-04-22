df9034bc10f0427fad3f1c8e924506dddeacf920
plger <pl.germain@gmail.com>
2021-04-22 09:13:02 +0200

    added documentation for example data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' @rdname SampleTranscript
+#' @name SampleTranscript
+#' @aliases SampleTranscript
+#'
+#' @title Example transcript sequence
+#'
+#' @description
+#' An artificial transcript sequence used for examples.
+#'
+#' @return a named character vector of length 1.
+NULL
+
+#' @rdname SampleKdModel
+#' @name SampleKdModel
+#' @aliases SampleKdModel
+#'
+#' @title Example KdModel (hsa-miR-155-5p)
+#'
+#' @description
+#' `KdModel` for hsa-miR-155-5p, based on Kd predictions from the CNN of
+#' [McGeary, Lin et al. (2019)](https://dx.doi.org/10.1126/science.aav1741).
+#'
+#' @return a `KdModel` object
+NULL
