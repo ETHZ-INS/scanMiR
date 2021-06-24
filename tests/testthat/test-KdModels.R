41a70e5c5ed6809ac1601239cd28ea3d53bcb12f
plger <pl.germain@gmail.com>
2021-06-24 19:46:45 +0200

    attempt to fix CI error

--- tests/testthat/test-KdModels.R
+++ tests/testthat/test-KdModels.R
@@ -27,15 +27,17 @@ mod <- getKdModel(kd=dummyKdData(), mirseq="TTAATGCTAATCGTGATAGGGGTT",
 
 test_that("KdModel construction works", {
   expect_s4_class(mod, "KdModel")
-  expect_equal(all(!is.na(mod$mer8) & !is.infinite(mod$mer8)), TRUE)
-  expect_equal(all(!is.na(mod$fl) & !is.infinite(mod$fl)), TRUE)
+  expect(all(!is.na(mod$mer8) & !is.infinite(mod$mer8)),
+         "Missing/infinite values in `mer8`")
+  expect(all(!is.na(mod$fl) & !is.infinite(mod$fl)),
+         "Missing/infinite values in `fl`")
   expect_type(summary(mod), "character")
   expect_type(capture_output(show(mod)), "character")
   expect_gt(mod$cor,0.9)
   expect_gt(cor(mod$mer8, SampleKdModel$mer8),0.9)
-  expect_equal(as.character(assignKdType(kmers$kmer, mod)$type),
+  expect_identical(as.character(assignKdType(kmers$kmer, mod)$type),
                kmers$type)
-  expect_equal(assignKdType(c("CTAGCATTAAGT","CTAGCATTACGT"), mod)$log_kd,
+  expect_identical(assignKdType(c("CTAGCATTAAGT","CTAGCATTACGT"), mod)$log_kd,
                c(-5083,-4108), tolerance=250)
 })
 
@@ -45,8 +47,10 @@ test_that("KdModel plotting works",{
 
 test_that("KdModelList operations work", {
   expect_s4_class(KdModelList(mod,SampleKdModel), "KdModelList")
-  expect_s4_class(ml <- c(mod,SampleKdModel), "KdModelList")
+  ml <- c(mod,SampleKdModel)
+  expect_s4_class(ml, "KdModelList")
   expect_s4_class(ml[[1]], "KdModel")
+  expect_s4_class(ml$`my-miRNA`, "KdModel")
   expect_s4_class(ml[2:1], "KdModelList")
   expect_equal(names(ml), c("my-miRNA", "hsa-miR-155-5p"))
 })
