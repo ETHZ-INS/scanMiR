07b505702588e41eeec0eba9ba2c81b87bafdc1f
michasou <michael.soutschek@hest.ethz.ch>
2021-08-02 16:45:41 +0000

    New 3p and utr/orf parameters

--- R/scanning.R
+++ R/scanning.R
@@ -694,9 +694,9 @@ getMatchTypes <- function(x, seed, checkWobble=TRUE){
   list(a=0.007726,
     b=0.5735,
     c=0.1810,
-    p3=0.04403,
-    coef_utr = -0.28019,
-    coef_orf = -0.08622,
+    p3=0.051,
+    coef_utr = -0.17106,
+    coef_orf = -0.21546,
     keepSiteInfo = TRUE
     )
 }
